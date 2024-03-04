668df68b7bee55ea20aa7eef380c75ddfb853fc9
andy_101398 <andy_101398@castech.com.tw>
2024-03-04 14:04:11 +0800

    feat: comments aws ecs config, then run to localhost

--- app/controller/video_controller.go
+++ app/controller/video_controller.go
@@ -9,7 +9,7 @@ import (
 	"io"
 	"log"
 
-	// "os/exec"
+	"os/exec"
 
 	"net/http"
 	"os"
@@ -18,7 +18,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -50,6 +50,7 @@ func UploadFile(c *gin.Context) {
 	fmt.Printf("File Size: %+v\n", handler.Size)
 	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
+	// TODO:用不到，備份用
 	// ============================== Store in local directory ==============================
 	// General: Create file in the temp folder
 	// dstPath := "./static/videos/" + handler.Filename
@@ -86,19 +87,6 @@ func UploadFile(c *gin.Context) {
 		UpdatedAt: time.Now(),
 	}
 
-	// _, err = database.QmgoConnection.InsertOne(context.Background(), video)
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, "MongoDB: Error Inserting the File")
-	// 	return
-	// } else {
-	// ============================== Reverse video ==============================
-	// 查詢到临时文件
-	// err = database.QmgoConnection.Find(context.Background(), bson.M{"title": insertVideo.Title, "vid": insertVideo.ID}).One(&videoResult)
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, "MongoDB: Error Finding the File")
-	// 	return
-	// }
-
 	tempVideo, err := os.Create("./tempVideo.mp4")
 	if err != nil {
 		fmt.Println("Error creating temporary file:", err)
@@ -107,10 +95,10 @@ func UploadFile(c *gin.Context) {
 
 	tempVideo.Write(insertVideo.VideoData)
 	tempVideo.Close()
-	// inputFilePath := tempVideo.Name()
 
+	// TODO:用不到，備份用
 	// 复制原始视频
-	// copyCmd := exec.Command("ffmpeg", "-i", inputFilePath, "-c:v", "copy", "copyVideo.mp4")
+	// copyCmd := exec.Command("ffmpeg", "-i", tempVideo.Name(), "-c:v", "copy", "copyVideo.mp4")
 	// copyCmd.Stdout = os.Stdout
 	// copyCmd.Stderr = os.Stderr
 
@@ -122,77 +110,59 @@ func UploadFile(c *gin.Context) {
 
 	// TODO:這裡暫時註解，加速測試
 	// 逆转复制的视频
-	// reverseCmd := exec.Command("ffmpeg", "-i", "tempVideo.mp4", "-vf", "reverse", "-af", "areverse", "reversedVideo.mp4")
-	// reverseCmd.Stdout = os.Stdout
-	// reverseCmd.Stderr = os.Stderr
+	reverseCmd := exec.Command("ffmpeg", "-i", "tempVideo.mp4", "-vf", "reverse", "-af", "areverse", "reversedVideo.mp4")
+	reverseCmd.Stdout = os.Stdout
+	reverseCmd.Stderr = os.Stderr
 
-	// err = reverseCmd.Run()
-	// if err != nil {
-	// 	fmt.Println("Error reversing video:", err)
-	// 	return
-	// }
+	err = reverseCmd.Run()
+	if err != nil {
+		fmt.Println("Error reversing video:", err)
+		return
+	}
 
 	// 串接视频
 	// finalCmd := exec.Command("ffmpeg", "-safe", "0", "-f", "concat", "copyVideo.mp4", "-c:v ", "copy", "-c:a ", "copy", "copyVideo2.mp4")
 	// finalCmd := exec.Command("ffmpeg", "-f", "concat", "-i", "../../tempVideo.mp4", "../../copyVideo.mp4")
 	// ffmpeg -i 要被串接的影音檔案路徑1 -i 要被串接的影音檔案路徑2 -i 要被串接的影音檔案路徑3 -filter_complex "[0:v][0:a][1:v][1:a][2:v][2:a]concat=n=3:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" 輸出的影片檔案路徑
 	// TODO:這裡暫時註解，加速測試
-	// finalCmd := exec.Command("ffmpeg", "-i", "./tempVideo.mp4", "-i", "./reversedVideo.mp4", "-filter_complex", "[0:v][0:a][1:v][1:a]concat=n=2:v=1:a=1[outv][outa]", "-map", "[outv]", "-map", "[outa]", "-n", "./flippedVideo.mp4")
-	// finalCmd.Stdout = os.Stdout
-	// finalCmd.Stderr = os.Stderr
+	finalCmd := exec.Command("ffmpeg", "-i", "./tempVideo.mp4", "-i", "./reversedVideo.mp4", "-filter_complex", "[0:v][0:a][1:v][1:a]concat=n=2:v=1:a=1[outv][outa]", "-map", "[outv]", "-map", "[outa]", "-n", "./flippedVideo.mp4")
+	finalCmd.Stdout = os.Stdout
+	finalCmd.Stderr = os.Stderr
 
 	// TODO:這裡暫時註解，加速測試
-	// err = finalCmd.Run()
-	// if err != nil {
-	// 	fmt.Println("Error concat video:", err)
-	// 	return
-	// }
+	err = finalCmd.Run()
+	if err != nil {
+		fmt.Println("Error concat video:", err)
+		return
+	}
 	// 删除临时文件
-	// if err := os.Remove("tempVideo.mp4"); err != nil {
-	// 	fmt.Println("deleting tempVideo fail", err)
-	// }
+	if err := os.Remove("tempVideo.mp4"); err != nil {
+		fmt.Println("deleting tempVideo fail", err)
+	}
 
-	// TODO:這裡暫時註解，加速測試
-	// data2, err := os.ReadFile("./reversedVideo.mp4")
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, "Error Reading the File")
-	// 	return
-	// }
-
-	// if err := os.Remove("reversedVideo.mp4"); err != nil {
-	// 	fmt.Println("deleting reversedVideo fail:", err)
-	// }
+	if err := os.Remove("reversedVideo.mp4"); err != nil {
+		fmt.Println("deleting reversedVideo fail:", err)
+	}
 
 	// MongoDB operation
 	file.Seek(0, 0) // Reset the file pointer to the beginning of the file
 	// MongoDB: Read the uploaded file data
-
 	// TODO:這裡暫時註解，加速測試
-	// data2, err := os.ReadFile("./flippedVideo.mp4")
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, "Error Reading the File")
-	// 	return
-	// }
-
-	// TODO:這裡暫時註解，加速測試
-	// if err := os.Remove("flippedVideo.mp4"); err != nil {
-	// 	fmt.Println("deleting flippedVideo fail", err)
-	// }
-
-	// FIXME:臨時加的
-	data2, err := os.ReadFile("./tempVideo.mp4")
+	flippedVideoData, err := os.ReadFile("./flippedVideo.mp4")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, "Error Reading the File")
 		return
 	}
-	if err := os.Remove("tempVideo.mp4"); err != nil {
-		fmt.Println("deleting tempVideo fail", err)
+
+	// TODO:這裡暫時註解，加速測試
+	if err := os.Remove("flippedVideo.mp4"); err != nil {
+		fmt.Println("deleting flippedVideo fail", err)
 	}
 
 	insertFlippedVideo := model.Video{
 		VID:       primitive.NewObjectID(),
 		Title:     "flippedVideo",
-		VideoData: data2,
+		VideoData: flippedVideoData,
 		UpdatedAt: time.Now(),
 	}
 
@@ -204,15 +174,15 @@ func UploadFile(c *gin.Context) {
 
 	// log.Println("insertFlippedVideo.Title: ", insertFlippedVideo.Title)
 	log.Println("insertFlippedVideo.ID: ", insertFlippedVideo.VID)
-	// ============================== Render to index.html ==============================
 
-	var videoResult model.Video
+	// TODO:用不到，備份用
+	// var videoResult model.Video
 	// MongoDB: find one specific video from mongodb
-	err = database.QmgoConnection.Find(context.Background(), bson.M{"vid": insertFlippedVideo.VID}).One(&videoResult)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, "MongoDB: Finding the insertFlippedVideo fail")
-		return
-	}
+	// err = database.QmgoConnection.Find(context.Background(), bson.M{"vid": insertFlippedVideo.VID}).One(&videoResult)
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, "MongoDB: Finding the insertFlippedVideo fail")
+	// 	return
+	// }
 
 	// MongoDB: Convert the VideoData to base64 encoding
 	videoBase64 := base64.StdEncoding.EncodeToString(insertFlippedVideo.VideoData)
--- database/mongo.go
+++ database/mongo.go
@@ -11,11 +11,11 @@ var err error
 func MongoDBinit() {
 	ctx := context.TODO()
 	QmgoConnection, err = qmgo.Open(ctx, &qmgo.Config{
-		// Uri:      "mongodb://localhost:27017/",
+		Uri: "mongodb://localhost:27017/",
 		// Uri:      "mongodb://root:root@db:27017/",
 		// Uri:      "mongodb://root:root@172.31.12.7:27017/",
 		// Uri:      "mongodb://root:root@172.31.18.14:27017/",
-		Uri:      "mongodb://root:root@172.31.3.255:27017/",
+		// Uri:      "mongodb://root:root@172.31.3.255:27017/",
 		Database: "meetingVideoHelper",
 		Coll:     "videoHelper",
 	})
