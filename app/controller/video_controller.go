a00f254e2a2da018dee4127facaaeb8a07ca6da4
ks0dcongra <andy_101398@castech.com.tw>
2024-05-10 01:47:13 +0800

    Delete redundant comments

--- app/controller/video_controller.go
+++ app/controller/video_controller.go
@@ -4,7 +4,6 @@ import (
 	"MeetingVideoHelper/app/contract"
 	"MeetingVideoHelper/app/service"
 	"encoding/base64"
-	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -53,7 +52,6 @@ func (vc *VideoController) DownloadVideo(c *gin.Context) {
 	}
 	defer os.Remove(filePath)
 	filePath = filePath[2:]
-	fmt.Println("filePathfilePathfilePath", filePath)
 	c.Header("Content-Disposition", "attachment; filename="+filePath)
 	c.Header("Content-Type", "video/mp4")
 	c.File(filePath)
@@ -106,120 +104,3 @@ func (vc *VideoController) UploadVideo(c *gin.Context) {
 		"videoData":     insertVideo.VideoData,
 	})
 }
-
-// func UploadFile2(c *gin.Context) {
-// 	// ============================== Get upload file ==============================
-// 	// maximum upload of 10 MB files
-// 	c.Request.ParseMultipartForm(10 << 20)
-
-// 	// get handler for filename, size and headers
-// 	file, _, err := c.Request.FormFile("myFile")
-// 	if err != nil {
-// 		fmt.Println("Error Retrieving the File")
-// 		fmt.Println(err)
-// 		c.HTML(http.StatusInternalServerError, "index.html", gin.H{
-// 			"err": "Error Retrieving the File",
-// 			"msg": "Uploaded File Successfully ",
-// 		})
-// 	}
-
-// 	defer file.Close()
-
-// 	// ============================== Create local file to store temp data ==============================
-// 	// reset the file pointer to the beginning of the file
-// 	file.Seek(0, 0)
-// 	// convert to byte
-// 	data, err := io.ReadAll(file)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, "Error Reading the File")
-// 		return
-// 	}
-
-// 	tempVideo, err := os.Create("./tempVideo.mp4")
-// 	if err != nil {
-// 		fmt.Println("Error creating temporary file:", err)
-// 		return
-// 	}
-
-// 	tempVideo.Write(data)
-// 	tempVideo.Close()
-
-// 	// reverse vedio
-// 	reverseCmd := exec.Command("ffmpeg", "-i", "tempVideo.mp4", "-vf", "reverse", "-af", "areverse", "reversedVideo.mp4")
-// 	reverseCmd.Stdout = os.Stdout
-// 	reverseCmd.Stderr = os.Stderr
-
-// 	err = reverseCmd.Run()
-// 	if err != nil {
-// 		fmt.Println("Error reversing video:", err)
-// 		return
-// 	}
-
-// 	// ffmpeg -i 要被串接的影音檔案路徑1 -i 要被串接的影音檔案路徑2 -i 要被串接的影音檔案路徑3 -filter_complex "[0:v][0:a][1:v][1:a][2:v][2:a]concat=n=3:v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" 輸出的影片檔案路徑
-// 	finalCmd := exec.Command("ffmpeg", "-i", "./tempVideo.mp4", "-i", "./reversedVideo.mp4", "-filter_complex", "[0:v][0:a][1:v][1:a]concat=n=2:v=1:a=1[outv][outa]", "-map", "[outv]", "-map", "[outa]", "-n", "./flippedVideo.mp4")
-// 	finalCmd.Stdout = os.Stdout
-// 	finalCmd.Stderr = os.Stderr
-
-// 	err = finalCmd.Run()
-// 	if err != nil {
-// 		fmt.Println("Error concat video:", err)
-// 		return
-// 	}
-// 	// 删除臨時文件
-// 	if err := os.Remove("tempVideo.mp4"); err != nil {
-// 		fmt.Println("deleting tempVideo fail", err)
-// 	}
-
-// 	if err := os.Remove("reversedVideo.mp4"); err != nil {
-// 		fmt.Println("deleting reversedVideo fail:", err)
-// 	}
-
-// 	// MongoDB: Read the uploaded file data
-// 	flippedVideoData, err := os.ReadFile("./flippedVideo.mp4")
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, "Error Reading the File")
-// 		return
-// 	}
-
-// 	if err := os.Remove("flippedVideo.mp4"); err != nil {
-// 		fmt.Println("deleting flippedVideo fail", err)
-// 	}
-
-// 	insertFlippedVideo := model.Video{
-// 		VID:       primitive.NewObjectID(),
-// 		Title:     "flippedVideo",
-// 		VideoData: flippedVideoData,
-// 		UpdatedAt: time.Now(),
-// 	}
-
-// 	_, err = database.QmgoConnection.InsertOne(context.Background(), insertFlippedVideo)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, "MongoDB: Error Inserting the insertFlippedVideo fail")
-// 		return
-// 	}
-
-// 	// log.Println("insertFlippedVideo.Title: ", insertFlippedVideo.Title)
-// 	log.Println("insertFlippedVideo.ID: ", insertFlippedVideo.VID)
-
-// 	// TODO:MongoDB的Find，用不到，備份用
-// 	// var videoResult model.Video
-// 	// MongoDB: find one specific video from mongodb
-// 	// err = database.QmgoConnection.Find(context.Background(), bson.M{"vid": insertFlippedVideo.VID}).One(&videoResult)
-// 	// if err != nil {
-// 	// 	c.JSON(http.StatusInternalServerError, "MongoDB: Finding the insertFlippedVideo fail")
-// 	// 	return
-// 	// }
-
-// 	// MongoDB: Convert the VideoData to base64 encoding
-// 	videoBase64 := base64.StdEncoding.EncodeToString(insertFlippedVideo.VideoData)
-
-// 	// uploaded file successfully
-// 	c.HTML(http.StatusOK, "index.html", gin.H{
-// 		"Msg":           "Uploaded File Successfully ",
-// 		"err":           "",
-// 		"tempVideoFile": videoBase64,
-// 		"videoID":       insertFlippedVideo.VID,
-// 		"videoTitle":    insertFlippedVideo.Title,
-// 		"videoData":     insertFlippedVideo.VideoData,
-// 	})
-// }
--- routes/routes.go
+++ routes/routes.go
@@ -2,18 +2,7 @@ package routes
 
 import (
 	"MeetingVideoHelper/app/controller"
-	// "fmt"
-	// "log"
-	// "os"
-
-	// "MeetingVideoHelper/app/model"
-	// "MeetingVideoHelper/database"
-	// "context"
-	// "net/http"
-
 	"github.com/gin-gonic/gin"
-	// "go.mongodb.org/mongo-driver/bson"
-	// "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func ApiRoutes(router *gin.Engine) {
@@ -39,49 +28,6 @@ func ApiRoutes(router *gin.Engine) {
 		// 透過 Gin 的 `File` 方法來提供靜態檔案服務
 		c.File(filePath)
 	})
-
-	// router.POST("/downloads", func(c *gin.Context) {
-	// 	videoID := c.PostForm("videoID")
-	// 	log.Println("videoID", videoID[10:34])
-	// 	videoDownload, err := primitive.ObjectIDFromHex(videoID[10:34])
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	// videoDownload, _ := primitive.ObjectIDFromHex(videoID)
-	// 	log.Println("videoDownload", videoDownload)
-
-	// 	var videoResult model.Video
-
-	// 	// MongoDB: find one specific video from mongodb
-	// 	err = database.QmgoConnection.Find(context.Background(), bson.M{"vid": videoDownload}).One(&videoResult)
-	// 	if err != nil {
-	// 		c.JSON(http.StatusInternalServerError, "MongoDB: Finding the FlippedVideo fail")
-	// 		return
-	// 	}
-
-	// 	tempVideo, err := os.CreateTemp("./", "tempVideo*.mp4")
-	// 	if err != nil {
-	// 		fmt.Println("Error creating temporary file:", err)
-	// 		return
-	// 	}
-
-	// 	tempVideo.Write(videoResult.VideoData)
-	// 	tempVideo.Close()
-	// 	filePath := tempVideo.Name()
-	// 	log.Println("filePathfilePathfilePathfilePathfilePathfilePath",filePath)
-	// 	// 設定標頭讓瀏覽器知道這是個下載檔案的請求
-	// 	c.Header("Content-Disposition", "attachment; filename="+filePath)
-	// 	c.Header("Content-Type", "video/mp4")
-	// 	// 透過 Gin 的 `File` 方法來提供靜態檔案服務
-	// 	c.File(filePath)
-
-	// 	e := os.Remove(filePath)
-	// 	if e != nil {
-	// 		log.Fatal(e)
-	// 	}
-
-	// 	c.HTML(200, "index.html", map[string]string{"title": "home"})
-	// })
 
 	router.POST("/downloads", controller.NewVideoController().DownloadVideo)
 
