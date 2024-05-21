f67bd2f4bce15e7a5e76fcd6b2852fb5dfb6d6c2
ks0dcongra <andy_101398@castech.com.tw>
2024-05-21 22:40:47 +0800

    feat: change the first English letter in the sentence to uppercase

--- app/contract/contract.go
+++ app/contract/contract.go
@@ -11,11 +11,11 @@ const (
 )
 
 var Message = map[int]string{
-	Success:                       "Upload video successfully",
-	ERROR_Upload_Video:            "Upload video fail",
-	ERROR_Download_Video:          "Download video fail",
-	ERROR_Download_Video_NotExist: "videoID not found",
-	ERROR_Video_To_Hex:            "video bytes to hex fail",
-	ERROR_Upload_Video_NotExist:   "video not found",
-	ERROR_Video_Format:            "video format has some problem",
+	Success:                       "Upload video successfully.",
+	ERROR_Upload_Video:            "Upload video fail.",
+	ERROR_Download_Video:          "Download video fail.",
+	ERROR_Download_Video_NotExist: "VideoID not found.",
+	ERROR_Video_To_Hex:            "Video bytes to hex fail.",
+	ERROR_Upload_Video_NotExist:   "Video not found.",
+	ERROR_Video_Format:            "Video format has some problem.",
 }
