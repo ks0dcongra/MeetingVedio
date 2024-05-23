68bb023c1514fa5a9838dab285a5f3302a6de024
ks0dcongra <andy_101398@castech.com.tw>
2024-05-24 00:55:32 +0800

    feat: remove global value of error

--- database/mongo.go
+++ database/mongo.go
@@ -6,9 +6,10 @@ import (
 )
 
 var QmgoConnection *qmgo.QmgoClient
-var err error
+
 
 func MongoDBinit() {
+	var err error
 	ctx := context.TODO()
 	QmgoConnection, err = qmgo.Open(ctx, &qmgo.Config{
 		// Uri: "mongodb://localhost:27017/",
